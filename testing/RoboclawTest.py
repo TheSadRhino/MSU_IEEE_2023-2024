6d3f3301a1833de88f5d3386f16318641ec47ca2
TheSadRhino <thesadrhino@gmail.com>
2023-12-23 21:50:09 -0600

    Initial Roboclaw Functionality and Testing

--- testing/RoboclawTest.py
+++ testing/RoboclawTest.py
@@ -5,24 +5,26 @@ from roboclaw import Roboclaw
 addressBack = 0x80
 addressFront = 0x81
 
-roboclawMultiplexer = Roboclaw("/dev/ttyACM0", 115200)
-roboclawMultiplexer.open()
+roboclawMultiplexer = Roboclaw("/dev/ttyS0", 115200)
 
-roboclawMultiplexer.driveM1Forward(addressFront, 127)
+if roboclawMultiplexer.open():
+    roboclawMultiplexer.driveM1Forward(addressFront, 127)
 
-time.sleep(1)
+    time.sleep(1)
 
-roboclawMultiplexer.driveM2Forward(addressFront, 127)
+    roboclawMultiplexer.driveM2Forward(addressFront, 127)
 
-time.sleep(1)
+    time.sleep(1)
 
-roboclawMultiplexer.driveM1Forward(addressBack, 127)
+    roboclawMultiplexer.driveM1Forward(addressBack, 127)
 
-time.sleep(1)
+    time.sleep(1)
 
-roboclawMultiplexer.driveM2Forward(addressBack, 127)
+    roboclawMultiplexer.driveM2Forward(addressBack, 127)
 
-time.sleep(1)
+    time.sleep(1)
 
-roboclawMultiplexer.driveForwardMixedMode(addressFront, 0)
-roboclawMultiplexer.driveForwardMixedMode(addressBack, 0)
+    roboclawMultiplexer.driveForwardMixedMode(addressFront, 0)
+    roboclawMultiplexer.driveForwardMixedMode(addressBack, 0)
+else:
+    print("Couldn't open the serial bus.")
