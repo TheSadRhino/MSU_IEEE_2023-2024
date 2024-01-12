51b4e90e5f6afc44430fc2425e415233cf923c8c
TheSadRhino <thesadrhino@gmail.com>
2024-01-11 18:37:22 -0600

    Add bytebuffer communication style to roboclaw

--- RoboclawTest.py
+++ RoboclawTest.py
@@ -1,30 +1,40 @@
 import time
 
-from roboclaw import Roboclaw
+import serial
+
+import roboclaw.bytebuffer
 
 addressBack = 0x80
 addressFront = 0x81
 
-roboclawMultiplexer = Roboclaw("/dev/serial0", 115200)
+serialDevice = serial.Serial(port="/dev/serial0", baudrate=115200)
+rc = roboclaw.bytebuffer.Roboclaw(serialDevice)
 
-if roboclawMultiplexer.open():
-    print(roboclawMultiplexer.driveM1Forward(addressFront, 64))
+rc.forward_m1(127)
+time.sleep(1)
+rc.forward_m1(0)
 
-    time.sleep(1)
 
-    print(roboclawMultiplexer.driveM2Forward(addressFront, 64))
-
-    time.sleep(1)
-
-    print(roboclawMultiplexer.driveM1Forward(addressBack, 64))
-
-    time.sleep(1)
-
-    print(roboclawMultiplexer.driveM2Forward(addressBack, 64))
-
-    time.sleep(1)
-
-    print(roboclawMultiplexer.driveForwardMixedMode(addressFront, 0))
-    print(roboclawMultiplexer.driveForwardMixedMode(addressBack, 0))
-else:
-    print("Couldn't open the serial bus.")
+# #roboclawMultiplexer = Roboclaw("/dev/serial0", 115200)
+#
+# if roboclawMultiplexer.open():
+#     print(roboclawMultiplexer.driveM1Forward(addressFront, 64))
+#
+#     time.sleep(1)
+#
+#     print(roboclawMultiplexer.driveM2Forward(addressFront, 64))
+#
+#     time.sleep(1)
+#
+#     print(roboclawMultiplexer.driveM1Forward(addressBack, 64))
+#
+#     time.sleep(1)
+#
+#     print(roboclawMultiplexer.driveM2Forward(addressBack, 64))
+#
+#     time.sleep(1)
+#
+#     print(roboclawMultiplexer.driveForwardMixedMode(addressFront, 0))
+#     print(roboclawMultiplexer.driveForwardMixedMode(addressBack, 0))
+# else:
+#     print("Couldn't open the serial bus.")
