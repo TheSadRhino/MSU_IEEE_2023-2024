5393d42c4c9714be65c25ed435e7deaeedb8e950
TheSadRhino <thesadrhino@gmail.com>
2024-01-13 19:47:53 -0600

    Add error testing for IMU

--- BNO085Test.py
+++ BNO085Test.py
@@ -8,24 +8,31 @@ from adafruit_bno08x import (
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
 
 bno.enable_feature(BNO_REPORT_GAME_ROTATION_VECTOR)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 while True:
-    time.sleep(0.25)
+    time.sleep(0.5)
     print("Game Rotation Vector Quaternion:")
-    gameI, gameJ, gameK, gameReal = bno.game_quaternion
-    print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (gameI, gameJ, gameK, gameReal)
-    )
+    try:
+        gameI, gameJ, gameK, gameReal = bno.game_quaternion
+
+        print(
+            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (gameI, gameJ, gameK, gameReal)
+        )
+    except Exception as e:
+        print("Error occurred in Game Rotation Vector: " + str(e))
     print("")
 
     print("Rotation Vector Quaternion:")
-    quatI, quatJ, quatK, quatReal = bno.quaternion  # pylint:disable=no-member
-    print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quatI, quatJ, quatK, quatReal)
-    )
+    try:
+        quatI, quatJ, quatK, quatReal = bno.quaternion  # pylint:disable=no-member
+        print(
+            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quatI, quatJ, quatK, quatReal)
+        )
+    except Exception as e:
+        print("Error occurred in Rotation Vector: " + str(e))
     print("")
