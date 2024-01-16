27ce5ccdf75b9a28136ac6ea6aa4ce576c90e060
TheSadRhino <thesadrhino@gmail.com>
2024-01-16 15:21:03 -0600

    In order to do testing, the files have to be in root

--- roboclaw/roboclaw.py
+++ roboclaw/roboclaw.py
@@ -379,8 +379,8 @@ class Roboclaw:
     def _clearCRC16(self) -> None:
         self.__crc16 = 0
 
-    def _updateCRC16SingleByte(self, byte: bytes) -> None:
-        self.__crc16 = self.__crc16 ^ ((int.from_bytes(byte, "big") & 0xFF) << 8)
+    def _updateCRC16SingleByte(self, byte) -> None:
+        self.__crc16 = self.__crc16 ^ (byte & 0xFF) << 8
         for bit in range(0, 8):
             if bool(self.__crc16 & 0x8000):
                 self.__crc16 = (self.__crc16 << 1) ^ 0x1021
