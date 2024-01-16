c49aa2afcbf0f0d7000536469cd24d60cc2faf00
TheSadRhino <thesadrhino@gmail.com>
2024-01-16 14:39:33 -0600

    Modify actions to allow robot data inside them and adjust roboclaw.py to include pack/unpack of bytes

---
+++ testing/CustomRoboclawTest.py
@@ -0,0 +1,13 @@
+import time
+
+import roboclaw.roboclaw
+
+addressBack = 0x80
+addressFront = 0x81
+
+rc = roboclaw.Roboclaw(addressBack, "/dev/serial0", 115200)
+rc.open()
+
+rc.driveM1Forward(64)
+time.sleep(3)
+rc.driveM1Forward(0)
