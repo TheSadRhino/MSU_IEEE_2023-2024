b406d3fea69375c3d32806c23d6cbd666f7cc973
TheSadRhino <thesadrhino@gmail.com>
2024-03-20 18:06:51 -0500

    Add servo constants

--- subsystems/configurations/RobotConstants.py
+++ subsystems/configurations/RobotConstants.py
@@ -1,3 +1,4 @@
+# --Sensors--
 # Address, GPIO pin, physical pin location, sensor type
 
 frontRightSideTOFSensorPins = (0x29, 20, 38, "VL53L0X")
@@ -18,3 +19,28 @@ lightSensorPins = (0x39, 7, 26, "AS7341")
 
 redButtonPins = (None, 17, 11, "Limit Switch")
 redLEDPins = (None, 27, 13, "LED")
+
+# --Servos--
+elevatorServoPin = 0
+elevatorServoUpPosition = 1700
+elevatorServoDownPosition = 1150
+
+sideIntakeServoPin = 1
+sideIntakeServoUpPosition = 2150
+sideIntakeServoDownPosition = 1350
+
+clawServoPin = 2
+clawServoClampPosition = 1250
+clawServoOpenPosition = 1350
+
+frontIntakeServoPin = 3
+frontIntakeServoUpPosition = 935
+frontIntakeServoDownPosition = 2135
+
+boosterAlignmentServoPin = 4
+boosterAlignmentServoUpPosition = 2055
+boosterAlignmentServoDownPosition = 825
+
+clawDeploymentServoPin = 5
+clawDeploymentServoUpPosition = 2175
+clawDeploymentServoDownPosition = 790
