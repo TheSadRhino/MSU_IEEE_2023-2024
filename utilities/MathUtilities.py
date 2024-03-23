d894db9511650d4c8bd911ba6b149128025f3471
TheSadRhino <thesadrhino@gmail.com>
2024-03-23 00:17:55 -0500

    No division by zero

--- utilities/MathUtilities.py
+++ utilities/MathUtilities.py
@@ -13,7 +13,10 @@ def quaternionToEuler(qr, qi, qj, qk, degrees: bool = False):
     sqk = qk ** 2
 
     _yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr))
-    _pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr))
+    if (sqi + sqj + sqk + sqr) != 0:
+        _pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr))
+    else:
+        _pitch = math.copysign(math.pi/2, -2.0 * (qi * qk - qj * qr))
     _roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr))
 
     if degrees:
