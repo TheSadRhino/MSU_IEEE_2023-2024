dae2f8cbe3641997c058a68d31dc6ed1958c35da
TheSadRhino <thesadrhino@gmail.com>
2024-03-23 05:16:20 -0500

    Change outtaking maneuvering

--- Round1Program.py
+++ Round1Program.py
@@ -30,55 +30,71 @@ robot.runAction(
     SeriesAction(
         [WaitForButtonPress(),
          ZeroGyroValues(),
-         SetDrivetrainVelocity(0, 0.175, 0),
-         WaitForLeftSideDistanceLessThan(150),
-         SetDrivetrainVelocity(0, 0, 0),
-         ParallelAction(
-             [DeployIntakesAction(),
-              SetSideIntakeVelocity(0.4),
-              SetFrontIntakeVelocity(0.5)]
-         ),
-         SetDrivetrainVelocity(0.025, -0.2, 0),
-         WaitAction(0.4),
-         ParallelAction(
-             [SetDrivetrainVelocity(-0.05, 0.15, 0),
-              WaitForLeftSideDistanceLessThan(150)]
-         ),
-         ParallelAction(
-             [RetractSideIntakeAction(),
-              SetSideIntakeVelocity(0),
-              WaitAction(0.5)]
-         ),
-         SetDrivetrainVelocity(-0.05, -0.05, 0),
-         SetDrivetrainVelocity(0.175, 0, 0),
-         WaitAction(0.75),
-         SetDrivetrainVelocity(-0.175, 0, 0),
-         WaitAction(0.75),
-         SetDrivetrainVelocity(0, -0.1, 0),
-         WaitAction(0.125),
-         SetDrivetrainVelocity(0.35, 0, 0),
-         ParallelAction(
-             [WaitAction(3.5),
-              RetractFrontIntakeAction(),
-              SetFrontIntakeVelocity(0)]
-         ),
-         SetDrivetrainVelocity(0, -0.175, 0),
-         WaitAction(0.25),
-         SetDrivetrainVelocity(-0.05, 0.04, -0.025),
-         ParallelAction(
-             [WaitAction(0.125),
-              DeployIntakesAction()]
-         ),
-         SetFrontIntakeVelocity(-0.3),
-         WaitAction(0.05),
-         SetFrontIntakeVelocity(0),
-         TurnForNegativeAngle(-110, 0.075),
-         SetSideIntakeVelocity(-0.25),
-         WaitAction(0.1),
-         SetSideIntakeVelocity(0)
-         ]
-    )
-)
+         AtomicAction(
+             [
+              SeriesAction(
+               [SetDrivetrainVelocity(0, 0.175, 0),
+                WaitForLeftSideDistanceLessThan(150),
+                SetDrivetrainVelocity(0, 0, 0),
+                ParallelAction(
+                    [DeployIntakesAction(),
+                     SetSideIntakeVelocity(0.4),
+                     SetFrontIntakeVelocity(0.5)]
+                ),
+                SetDrivetrainVelocity(0.025, -0.2, 0),
+                WaitAction(0.4),
+                ParallelAction(
+                    [SetDrivetrainVelocity(-0.05, 0.15, 0),
+                     WaitForLeftSideDistanceLessThan(150)]
+                ),
+                ParallelAction(
+                    [RetractSideIntakeAction(),
+                     SetSideIntakeVelocity(0),
+                     WaitAction(0.5)]
+                ),
+                SetDrivetrainVelocity(-0.05, -0.05, 0),
+                SetDrivetrainVelocity(0.175, 0, 0),
+                WaitAction(0.75),
+                SetDrivetrainVelocity(-0.175, 0, 0),
+                WaitAction(0.75),
+                SetDrivetrainVelocity(0, -0.1, 0),
+                WaitAction(0.125),
+                SetDrivetrainVelocity(0.35, 0, 0),
+                ParallelAction(
+                    [WaitAction(3.5),
+                     RetractFrontIntakeAction(),
+                     SetFrontIntakeVelocity(0)]
+                ),
+                SetDrivetrainVelocity(0, -0.175, 0),
+                WaitAction(0.25),
+                SetDrivetrainVelocity(-0.05, 0.04, -0.025),
+                ParallelAction(
+                    [WaitAction(0.125),
+                     DeployIntakesAction()]
+                ),
+                SetFrontIntakeVelocity(-0.3),
+                WaitAction(0.05),
+                SetFrontIntakeVelocity(0),
+                TurnForNegativeAngle(-110, 0.075),
+                SetSideIntakeVelocity(-0.25),
+                WaitAction(0.1),
+                SetSideIntakeVelocity(0),
+                SetDrivetrainVelocity(0.1, 0, 0),
+                WaitAction(1),
+                ParallelAction(
+                    [SetDrivetrainVelocity(0, 0, 0.05),
+                     RetractSideIntakeAction(),
+                     RetractFrontIntakeAction(),
+                     WaitAction(0.5)]
+                ),
+                SetDrivetrainVelocity(0.25, 0, 0),
+                WaitAction(0.2),
+                SetDrivetrainVelocity(0, 0, 0)
+                ]
+              ),
+              WaitForButtonPress()]
+        )]
+    ))
 
 
 
