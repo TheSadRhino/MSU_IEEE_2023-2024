1e461724f53d07f9f917c06c18f0dea3c76062e0
TheSadRhino <thesadrhino@gmail.com>
2024-03-23 04:16:23 -0500

    Add gyro test

--- GyroPrintout.py
+++ GyroPrintout.py
@@ -2,10 +2,13 @@ from subsystems.Robot import Robot
 
 robot = Robot()
 robot._readInput()
+robot._updateSystem()
 
 robot.zeroAngles()
 
 while True:
     robot._readInput()
+    robot._updateSystem()
+
     yaw, pitch, roll = robot.getAngles()
     print("Yaw: " + yaw + " | Pitch: " + pitch + " | Roll: " + roll)
