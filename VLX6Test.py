9095f9c82075e132123fb8b6971c0216f61fadd5
TheSadRhino <thesadrhino@gmail.com>
2024-08-26 23:13:28 -0500

    add i2c multiplexing rev4

--- VLX6Test.py
+++ VLX6Test.py
@@ -36,8 +36,11 @@ if __name__ == "__main__":
     tca = adafruit_tca9548a.TCA9548A(i2c)
 
     # For each sensor, create it using the TCA9548A channel instead of the I2C object
-    tsl1 = VL6180X(tca[0])
-    tsl2 = VL6180X(tca[1])
+    tsl1 = VL6180X(tca[1])
+    tsl2 = VL6180X(tca[4])
+
+    tsl1.start_continuous()
+    tsl2.start_continuous()
 
     # After initial setup, can just use sensors as normal.
     while True:
